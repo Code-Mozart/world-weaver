998af933ca79a87ecf6280c8c3ac143172cb0d9f
Code-Mozart <thecodemozart@gmail.com>
2024-12-22 14:36:48 +0100

    temporarily added demo objects in world

--- src/lib/factories/world-factory.ts
+++ src/lib/factories/world-factory.ts
@@ -1,6 +1,6 @@
 import type { WorldDocument } from "$lib/types/documents/world-document";
 import { GroundType } from "$lib/types/ground-type";
-import type { World } from "$lib/types/world";
+import type { Point, Polygon, World } from "$lib/types/world";
 import { createId } from "@paralleldrive/cuid2";
 
 export namespace WorldFactory {
@@ -28,5 +28,39 @@ export namespace WorldFactory {
       authors: [],
       groundType: GroundType.Land,
     };
+  }
+
+  export function createDemoWorld(): World {
+    const world = createEmpty();
+    addDemoObjects(world);
+    return world;
+  }
+
+  export function addDemoObjects(world: World) {
+    world.coastlines.push({
+      id: -1,
+      temporaryCuid: createId(),
+
+      name: "Continent 1",
+      groundType: GroundType.Land,
+      shape: createPolygon([
+        { x: 0, y: 0 },
+        { x: 100, y: 0 },
+        { x: 100, y: 100 },
+        { x: 0, y: 100 },
+      ]),
+    });
+  }
+
+  function createPolygon(positions: { x: number; y: number }[]): Polygon {
+    return {
+      id: -1,
+      temporaryCuid: createId(),
+      points: positions.map(position => createPoint(position.x, position.y)),
+    };
+  }
+
+  function createPoint(x: number, y: number): Point {
+    return { id: -1, temporaryCuid: createId(), x, y };
   }
 }
--- src/routes/worlds/[id]/+page.server.ts
+++ src/routes/worlds/[id]/+page.server.ts
@@ -1,6 +1,9 @@
+import { WorldFactory } from "$lib/factories/world-factory";
 import { loadWorld } from "$lib/io/reading/world-loader";
 import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ params }) => {
-  return { world: await loadWorld(params.id) };
+  const world = await loadWorld(params.id);
+  WorldFactory.addDemoObjects(world);
+  return { world };
 };
