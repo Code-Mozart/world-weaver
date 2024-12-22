da78c02fd413df39ad7b0f52368805684528d071
Code-Mozart <thecodemozart@gmail.com>
2024-12-22 16:21:41 +0100

    update theme on media change

--- src/lib/view/themes/stylesheet-theme.ts
+++ src/lib/view/themes/stylesheet-theme.ts
@@ -11,24 +11,56 @@ export class StylesheetTheme implements Theme {
   coastline: Theme.Coastline;
   rivers: Theme.Rivers;
 
+  style: CSSStyleDeclaration;
+
   constructor(style: CSSStyleDeclaration) {
+    this.style = style;
+    this.initializeObserver();
+
     this.groundTypes = {
-      land: { fillColor: getVariable("--land-fill-color", style) },
-      water: { fillColor: getVariable("--water-fill-color", style) },
-      lava: { fillColor: getVariable("--lava-fill-color", style) },
-      void: { fillColor: getVariable("--void-fill-color", style) },
+      land: { fillColor: getVariable("--land-fill-color", this.style) },
+      water: { fillColor: getVariable("--water-fill-color", this.style) },
+      lava: { fillColor: getVariable("--lava-fill-color", this.style) },
+      void: { fillColor: getVariable("--void-fill-color", this.style) },
     };
     this.point = {
-      radius: parseFloat(getVariable("--point-radius", style)),
+      radius: parseFloat(getVariable("--point-radius", this.style)),
     };
     this.coastline = {
       outline: {
-        color: getVariable("--coastline-outline-color", style),
-        width: parseFloat(getVariable("--coastline-outline-width", style)),
+        color: getVariable("--coastline-outline-color", this.style),
+        width: parseFloat(getVariable("--coastline-outline-width", this.style)),
       },
     };
     this.rivers = {
-      fillColor: getVariable("--river-fill-color", style),
+      fillColor: getVariable("--river-fill-color", this.style),
+    };
+  }
+
+  protected initializeObserver() {
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", event => {
+      this.update();
+    });
+  }
+
+  protected update() {
+    this.groundTypes = {
+      land: { fillColor: getVariable("--land-fill-color", this.style) },
+      water: { fillColor: getVariable("--water-fill-color", this.style) },
+      lava: { fillColor: getVariable("--lava-fill-color", this.style) },
+      void: { fillColor: getVariable("--void-fill-color", this.style) },
+    };
+    this.point = {
+      radius: parseFloat(getVariable("--point-radius", this.style)),
+    };
+    this.coastline = {
+      outline: {
+        color: getVariable("--coastline-outline-color", this.style),
+        width: parseFloat(getVariable("--coastline-outline-width", this.style)),
+      },
+    };
+    this.rivers = {
+      fillColor: getVariable("--river-fill-color", this.style),
     };
   }
 }
